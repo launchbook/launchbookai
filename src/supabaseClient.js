192e2ee89002d5ffe07f39853b456fa3a4e6dace
launchbook <launchbookai@gmail.com>
2025-07-06 13:20:24 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,3 +5,4 @@ const supabaseUrl = 'https://wgvquzflchtmgdgwugrq.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndndnF1emZsY2h0bWdkZ3d1Z3JxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3ODcwNTYsImV4cCI6MjA2NzM2MzA1Nn0.xMIbM0AYW24qerEeAi3SDTxrNOtO5tWUYHMudxNwTjg'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
